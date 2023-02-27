28f1af6971e5c65f9175ed7840b06131b928dae8
Duong Quoc Viet <ntysux@gmail.com>
2023-02-27 18:15:28 +0700

    use chakra & custom theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    app: {
+      black: {
+        dark: '#49474B',
+        light: '#68666B'
+      },
+      gray: {
+        dark: '88898C'
+      },
+      teal: '#5CF2E3'
+    }
+  }
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
