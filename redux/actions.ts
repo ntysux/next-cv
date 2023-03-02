f4af4c4a5563544e69675c04127bbacca613fbba
Duong Quoc Viet <ntysux@gmail.com>
2023-03-02 14:03:53 +0700

    create: rename action

--- redux/actions.ts
+++ redux/actions.ts
@@ -6,3 +6,8 @@ export const setColorTheme = (color: string) => ({
   type: 'SET_COLOR',
   payload: {color}
 });
+
+export const rename = (name: string) => ({
+  type: 'RENAME',
+  payload: {name}
+});
