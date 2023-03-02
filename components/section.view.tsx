a90fff5c0677958deecd745bfe5a55578a81907f
Duong Quoc Viet <ntysux@gmail.com>
2023-03-02 21:28:48 +0700

    create ui: section view

---
+++ components/section.view.tsx
@@ -0,0 +1,81 @@
+import * as C from "@chakra-ui/react";
+import * as TB from "@tabler/icons-react";
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
+import SectionMenuActions from "./section.menu-options";
+
+export default function SectionView() {
+  const { isOpen, onOpen, onClose } = C.useDisclosure();
+  const { color, mode } = useSelector((state: RootState) => state.cv);
+
+  return (
+    <>
+      <C.Accordion defaultIndex={[0, 1]} allowMultiple>
+        <C.AccordionItem>
+          {({ isExpanded }) => (
+            <>
+              <C.HStack>
+                <C.AccordionButton>Sec 1</C.AccordionButton>
+                <C.Box
+                  onClick={() => mode && onOpen()}
+                  cursor={mode ? 'pointer' : 'default'}
+                  p={mode ? 1 : 0.5}
+                  border='2px'
+                  rounded='full'
+                  transition='all 0.3s'
+                  w={isExpanded ? '0px': '24px'}
+                  bg={isExpanded ? color : 'white'}
+                  borderColor={isExpanded ? color : 'app.gray.dark'}
+                />
+              </C.HStack>
+              <C.AccordionPanel>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat.
+              </C.AccordionPanel>
+            </>
+          )}
+        </C.AccordionItem>
+      </C.Accordion>
+
+      <C.Modal isOpen={isOpen} onClose={onClose} closeOnOverlayClick={false}>
+        <C.ModalOverlay />
+        <C.ModalContent maxW='container.lg'>
+          <C.ModalHeader>
+            <C.Center position='relative'>
+              <C.HStack
+                bg='app.black.light'
+                p={1}
+                pl={3}
+                rounded='full'
+              >
+                <C.Text fontSize='sm' color='white'>Untited</C.Text>
+                <SectionMenuActions />
+              </C.HStack>
+              <C.IconButton
+                position='absolute'
+                right='0'
+                onClick={onClose}
+                aria-label='close section'
+                variant='close'
+                icon={<TB.IconX size='16px' strokeWidth='3' />}
+                size='xs'
+              />
+            </C.Center>
+          </C.ModalHeader>
+          <C.ModalBody>
+
+          </C.ModalBody>
+          <C.ModalFooter>
+            <C.Center w='full'>
+              <C.Button size='sm' onClick={onClose} variant='solidBlack' w='2xs'>
+                Tạo
+              </C.Button>
+            </C.Center>
+          </C.ModalFooter>
+        </C.ModalContent>
+      </C.Modal>
+    </>
+  );
+}
