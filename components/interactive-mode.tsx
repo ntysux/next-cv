e4fdbad6ecee1bc74139f3780973999aee814936
Duong Quoc Viet <ntysux@gmail.com>
2023-03-02 15:30:18 +0700

    change mode use redux state

--- components/interactive-mode.tsx
+++ components/interactive-mode.tsx
@@ -1,7 +1,17 @@
 import * as C from "@chakra-ui/react";
 import * as TB from "@tabler/icons-react";
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
+import { changeMode } from "@/redux/actions";
 
 export default function InteractiveMode() {
+  const dispatch = useDispatch();
+  const { mode } = useSelector((state: RootState) => state.cv);
+
+  function currentColor(mode: boolean) {
+    return mode ? '#5CF2E3' : 'white'
+  }
+
   return (
     <>
       <C.Center>
@@ -10,14 +20,22 @@ export default function InteractiveMode() {
             as={C.IconButton}
             aria-label='set mode'
             variant='unstyledGrayHoverWhite'
-            icon={<TB.IconEye size='20px' />}
+            icon={mode ? <TB.IconPencil size='20px' /> : <TB.IconEye size='20px' />}
             size='sm'
           />
           <C.MenuList>
-            <C.MenuItem icon={<TB.IconEye size='18px' />} command='Ctrl + V'>
+            <C.MenuItem
+              icon={<TB.IconEye size='18px' color={currentColor(!mode)} />}
+              command='Ctrl + V'
+              onClick={() => dispatch(changeMode(false))}
+            >
               Chế độ xem
             </C.MenuItem>
-            <C.MenuItem icon={<TB.IconPencil size='18px' />} command='Ctrl + E'>
+            <C.MenuItem
+              icon={<TB.IconPencil size='18px' color={currentColor(mode)} />}
+              command='Ctrl + E'
+              onClick={() => dispatch(changeMode(true))}
+            >
               Chế độ chỉnh sửa
             </C.MenuItem>
           </C.MenuList>
