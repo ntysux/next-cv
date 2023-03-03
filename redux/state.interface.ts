d351699d4d86b636e28ddf6dbbba71ba11670e05
Duong Quoc Viet <ntysux@gmail.com>
2023-03-03 20:40:47 +0700

    interface of redux state

---
+++ redux/state.interface.ts
@@ -0,0 +1,34 @@
+export interface TableLayout {
+  th: string[],
+  td: any[]
+}
+export interface BasicLayout {
+  name?: string,
+  value?: string
+}
+export interface Image {
+  url: string,
+  align: 'left' | 'right' | 'top'
+}
+export interface Layout {
+  table?: TableLayout,
+  basic?: BasicLayout,
+  simple?: string,
+  image?: Image,
+  notes?: string
+}
+export interface Section {
+  name: string,
+  chil?: Layout[]
+}
+export interface Cv {
+  name: string,
+  color: string,
+  mode: boolean,
+  pin: number | null,
+  section: Section[]
+}
+export interface Action {
+  type: string,
+  payload?: any
+}
