ab5383be7ccf7cf6bd9079247211e8d74947c21f
Duong Quoc Viet <ntysux@gmail.com>
2023-03-03 11:25:22 +0700

    render section view

--- pages/cv/[id].tsx
+++ pages/cv/[id].tsx
@@ -1,7 +1,20 @@
 import * as C from "@chakra-ui/react";
 import Dashboard from "@/components/dashboard";
+import SectionView from "@/components/section.view";
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
 
 export default function CvPage() {
+  const { section } = useSelector((state: RootState) => state.cv);
+
+  interface Map {
+    array: any[],
+    render: (color: string, key: number) => JSX.Element
+  }
+  
+  const Map = ({array, render}: Map) =>
+    <>{array.map((color, key) => render(color, key))}</>
+
   return (
     <>
       <C.Flex
@@ -9,8 +22,11 @@ export default function CvPage() {
         bgSize='17px 17px'
         bgGradient='radial(#88898C 8%, transparent 10%)'
       >
-        <C.Container maxW='container.md'>
+        <C.Container maxW='container.lg'>
           <Dashboard />
+          <Map array={section} render={(item, key) => 
+            <SectionView key={key} value={{item, key}} />
+          }/>
         </C.Container>
       </C.Flex>
     </>
