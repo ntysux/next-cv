8aabda3f6e2514588f4bf523936dec5a2598684d
Duong Quoc Viet <ntysux@gmail.com>
2023-03-03 21:37:57 +0700

    update: type of section & case logic

--- redux/reducer.branch.ts
+++ redux/reducer.branch.ts
@@ -1,17 +1,13 @@
-import { Action, Section } from "./state.interface"
+import { Action, Section } from "./state.interface";
 
-const branchReducer = (section: Section[] = [], action: Action) => {
+const branchReducer = (section: Section = {}, action: Action) => {
   switch(action.type) {
     case 'NEW_BRANCH_SECTION':
-      return [...section, {name: 'Untited'}]
+      return {name: 'Untited'}
     case 'CANCEL_BRANCH_SECTION':
-      return [...section.slice(0, section.length - 1)]
+      return {}
     case 'RENAME_BRANCH_SECTION':
-      return [
-        ...section.slice(0, action.payload.index),
-        {...section[action.payload.index], name: action.payload.name},
-        ...section.slice(action.payload.index + 1)
-      ]
+      return {...section, name: action.payload.name}
     default:
       return section
   }
