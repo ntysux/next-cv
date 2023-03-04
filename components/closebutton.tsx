32a40ea96497b64fed34edf7585125a9deb0c12a
Duong Quoc Viet <ntysux@gmail.com>
2023-03-04 15:27:21 +0700

    new: CloseButton

--- components/closebutton.tsx
+++ components/closebutton.tsx
@@ -1,15 +1,29 @@
 import { IconButton } from "@chakra-ui/react";
 import { IconX } from "@tabler/icons-react";
 
-export function ModalCloseButton({ onClick }: {onClick: () => any}) {
+export function ModalCloseButton({ onClick }: {onClick?: () => any}) {
   return (
     <>
       <IconButton
+        aria-label='close section'
+        variant='closeModal'
         position='absolute'
         right='0'
         onClick={onClick}
-        aria-label='close section'
+        icon={<IconX size='16px' strokeWidth='3' />}
+        size='xs'
+      />
+    </>
+  );
+}
+
+export function CloseButton({ onClick }: {onClick?: () => any}) {
+  return (
+    <>
+      <IconButton
+        aria-label='close'
         variant='close'
+        onClick={onClick}
         icon={<IconX size='16px' strokeWidth='3' />}
         size='xs'
       />
