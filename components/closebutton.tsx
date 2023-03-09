596af9789ba470462a927ed61cfde62c6261e73e
Duong Quoc Viet <ntysux@gmail.com>
2023-03-09 18:25:08 +0700

    chore: style update

--- components/closebutton.tsx
+++ components/closebutton.tsx
@@ -5,18 +5,16 @@ interface Props {
   onClick?: () => void
 }
 
-export const DialogCloseButton = ({ onClick }: Props) =>
+export const DialogCloseButton = ({onClick}: Props) =>
   <IconButton
     aria-label='close dialog'
     variant='closeDialog'
-    position='absolute'
-    right='0'
     size='xs'
     onClick={onClick}
     icon={<IconX size='16px' strokeWidth='3' />}
   />
 
-export const CloseButtonDefault = ({ onClick }: Props) =>
+export const CloseButtonDefault = ({onClick}: Props) =>
   <IconButton
     aria-label='close'
     variant='close'
