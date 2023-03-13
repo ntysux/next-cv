d893a762ed09bbcf5e6bebda7e2805614795d86c
Duong Quoc Viet <ntysux@gmail.com>
2023-03-13 12:24:00 +0700

    add heading & use uuid

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import { Button, Flex, HStack } from "@chakra-ui/react";
-import Search from "@/components/search";
+import { Button, Flex, Heading, Stack, Text } from "@chakra-ui/react";
+import { v4 as uuidv4 } from "uuid"
 import Link from "next/link";
+import Search from "@/components/search";
 
 export default function HeroPage() {
   return (
@@ -12,16 +13,30 @@ export default function HeroPage() {
         bgSize='17px 17px'
         bgGradient='radial(#88898C 8%, transparent 10%)'
       >
-        <HStack>
-          <Button
-            as={Link}
-            href='/cv/something'
-            variant='solidBlack'
+        <Stack spacing={4}>
+          <Heading
+            textAlign='center'
+            fontFamily='Quicksand'
+            fontWeight='300'
           >
-            Bắt đầu
-          </Button>
-          <Search />
-        </HStack>
+            Next <Text as='span' color='app.teal' fontWeight='700'>CV</Text>
+          </Heading>
+          <Stack direction={{md: 'row', base: 'column'}}>
+            <Button
+              as={Link}
+              href={`cv/${uuidv4()}`}
+              variant='solidBlack'
+            >
+              Bắt đầu
+            </Button>
+            <Button
+              variant='outlineBlack'
+            >
+              Đăng nhập
+            </Button>
+            <Search />
+          </Stack>
+        </Stack>
       </Flex>
     </>
   );
